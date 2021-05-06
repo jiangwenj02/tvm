9fa10d8bdf214f828280291553f8340aa41ac290
jiangwenj02 <jiangwenj02@gmail.com>
2021-05-06 14:57:11 +0800

    fix

--- conda/recipe/install_tvm_python.sh
+++ conda/recipe/install_tvm_python.sh
@@ -20,4 +20,4 @@ set -e
 set -u
 
 cd ${SRC_DIR}/python
-${PYTHON} setup.py install --single-version-externally-managed --record=/tmp/record.txt
+python setup.py install --single-version-externally-managed --record=/tmp/record.txt
